a2336be0139ec68dfc64d844a68a57af355bbcf1
amirhamjacse <amirhamza824@gmail.com>
2025-01-27 17:49:45 +0600

    added templates

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -118,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+# settings.py
+
+STATIC_URL = '/static/'
+
+# Add this if it's not already there
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Or wherever your static files are located
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
