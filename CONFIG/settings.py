ce592f652d9aa04afbce3fb82e2d838950926966
amirhamjacse <amirhamza824@gmail.com>
2025-02-03 13:50:24 +0600

    updated

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -163,6 +163,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://deepmindacademy.com',
+    'https://www.deepmindacademy.com',  # if you're using both www and non-www versions
+    
+]
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
